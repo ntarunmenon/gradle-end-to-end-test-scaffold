99374d9e378fb3db1846599c4a3178c3bd372a69
arun.menon <ntarunmenon@gmail.com>
2017-04-05 16:02:15 +0400

    Completing scaffold

---
+++ src/end-to-end-test/java/org/arunm/EndtoEndIntegrationTest.java
@@ -0,0 +1,12 @@
+package org.arunm;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(EndToEndTest.class)
+public class EndtoEndIntegrationTest {
+ 
+    @Test
+    public void sampleIntegrationTest() {
+        System.out.println("Category: EndtoEndTest");
+    }
+}
